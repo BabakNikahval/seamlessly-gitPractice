59d110e5c8d74978c8d3a69fc4694daf12662271
CrazyDevo <adam@cybertekschool.com>
2022-03-25 15:21:36 -0400

    Conflict.java is created

---
+++ src/test/java/net/seamlessly/conflict/Conflict.java
@@ -0,0 +1,11 @@
+package net.seamlessly.conflict;
+
+import net.seamlessly.utilities.Driver;
+import org.junit.Test;
+
+public class Conflict {
+    @Test
+    public void test(){
+        System.out.println(Driver.getDriver().getCurrentUrl());
+    }
+}
